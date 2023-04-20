60974975f95ac385269cd399eabdda9d244a414d
Maryna-Harasko <mngarasko@gmail.com>
2023-04-21 00:40:13 +0300

    task 1

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button
+          key={option}
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
